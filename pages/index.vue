626ddc9bf603b5244003c41838aa47ad5e2cc5de
planet★箒星 <26595148+planethouki@users.noreply.github.com>
2021-08-12 18:47:19 +0900

    mypage split

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,19 @@
 <template>
   <div class="container">
-    <template v-if="isLoading">
-      <b-spinner type="grow" label="Spinning"></b-spinner>
-    </template>
-    <template v-else-if="nem2 === null">
-      {{ message }}
-    </template>
-    <template v-else>
-      <h1>NFT</h1>
-      <nft-balance />
-      <nft-all-list />
-      <p>transfer</p>
-      <p>buy</p>
-    </template>
+    <h1>NFT</h1>
+    <nft-all-list />
   </div>
 </template>
 
 <script>
-import NftBalance from '~/components/NftBalance.vue'
 import NftAllList from '~/components/NftAllList.vue'
 
 export default {
-  components: { NftAllList, NftBalance },
+  components: { NftAllList },
   data () {
-    return {
-      isLoading: true,
-      message: '',
-      nem2: null
-    }
+    return {}
   },
   mounted () {
-    if (window.nem2 === undefined) {
-      this.isLoading = false
-      this.message = `nem2 not found`
-      return
-    }
-
-    this.nem2 = window.nem2
-    this.nem2
-      .getAccountInfo()
-      .then(({ networkType, generationHash, publicKey, addressPlain, networkProperties }) => {
-        this.$store.commit('setSelfAddress', addressPlain)
-        this.isLoading = false
-      })
   }
 }
 </script>
---
+++ pages/mypage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <template v-if="isLoading">
+      <b-spinner type="grow" label="Spinning"></b-spinner>
+    </template>
+    <template v-else-if="nem2 === null">
+      {{ message }}
+    </template>
+    <template v-else>
+      <h1>NFT</h1>
+      <nft-balance />
+      <p>transfer</p>
+      <p>buy</p>
+    </template>
+  </div>
+</template>
+
+<script>
+import NftBalance from '~/components/NftBalance.vue'
+
+export default {
+  components: { NftBalance },
+  data () {
+    return {
+      isLoading: true,
+      message: '',
+      nem2: null
+    }
+  },
+  mounted () {
+    if (window.nem2 === undefined) {
+      this.isLoading = false
+      this.message = `nem2 not found`
+      return
+    }
+
+    this.nem2 = window.nem2
+    this.nem2
+      .getAccountInfo()
+      .then(({ networkType, generationHash, publicKey, addressPlain, networkProperties }) => {
+        this.$store.commit('setSelfAddress', addressPlain)
+        this.isLoading = false
+      })
+  }
+}
+</script>
+
+<style>
+</style>
